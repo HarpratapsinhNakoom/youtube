0142b7291abdd85975d1e4244297871d32a0d375
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-06 17:57:37 +0530

    added react-router routes for every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-// import Cart from "./pages/Cart";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-// import SingleProduct from "./components/SingleProduct";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import SearchProducts from './pages/SearchProducts'
+import SingleProduct from "./components/SingleProduct";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SearchProducts from './pages/SearchProducts'
 
 const App = () => {
   return(
     <>
-      <Home />
-      {/* <SearchProducts /> */}
-      {/* <SingleProduct /> */}
-      {/* <Login />
-      <Register/> */}
-      {/* <Cart /> */}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/products" element={<SearchProducts />}/>
+        <Route path="/product" element={<SingleProduct />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
     </>
   );
 };
